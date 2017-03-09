f38b0f00a1726aa7519408cc517c939b80704297
jmtcsngr <jmtc@sanger.ac.uk>
2017-03-09 11:24:38 +0000

    move controller setup to new module

--- npg_sentry.js
+++ npg_sentry.js
@@ -8,7 +8,12 @@
  */
 
 const config = require('./lib/config');
-let opts = config.provide(config.fromCommandLine);
+let opts;
+if ( module.parent ) {
+  opts = config.provide();
+} else {
+  opts = config.provide(config.fromCommandLine);
+}
 const logger = require('./lib/logger');
 
 const fs = require('fs');
@@ -20,8 +25,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
 
-const constants = require('./lib/constants');
-const model = require('./lib/model');
+const authorisation_controller = require('./lib/authorisation_controller');
 
 const port = opts.get('port');
 
@@ -49,7 +53,6 @@ if (opts.get('ssl')) {
   serv = http.createServer(app);
 }
 
-
 app.use(helmet({
   hsts: false
 }));
@@ -60,69 +63,7 @@ app.use(logger.connectLogger(logger, { level: 'auto' }));
 
 app.use(bodyParser.json());
 
-app.post('/createToken', function(req, res, next) {
-  // Does not expect any request body.
-  // Generates a random 32 character string and enters it
-  // into the db.
-  // Returns the new document as an application/json body.
-  let user = req.headers[constants.USER_ID_HEADER];
-
-  model.createToken(user, constants.WEB_TOKEN_CREATION_MSG).then(function(response) {
-    res.status(200).json(response);
-  }, next);
-});
-
-app.post('/revokeToken', function(req, res, next) {
-  // Expects body of application/json containing only the token
-  // which is to be rejected. Updates the document in db so that
-  // the 'status' field is set to 'revoked'.
-  // Returns the updated document in  an application/json body.
-  let user = req.headers[constants.USER_ID_HEADER];
-  let token;
-
-  try {
-    token = req.body.token;
-  } catch (e) {
-    next(e);
-  }
-
-  model.revokeToken(user, token, constants.WEB_TOKEN_REVOCATION_MSG).then(function(row) {
-    res.status(200).json(row);
-  }, next);
-});
-
-app.post('/checkToken', function(req, res, next) {
-  // Expects application/json request body to include a token and
-  // an array of group ids. Finds the user owning that token from db,
-  // then finds the groups that user is a member of.
-  // Returns {ok: true} if user's group membership is a superset of
-  // groups specified in request body.
-  let token = req.body.token;
-  let groups = req.body.groups;
-
-  model.checkToken(groups, token).then(function(decision) {
-    res.status(200).json({ok: decision});
-  }, next);
-});
-
-app.post('/checkUser', function(req, res, next) {
-  let user = req.body.user;
-  let groups = req.body.groups;
-
-  model.checkUser(groups, user).then(function(decision) {
-    res.status(200).json({ok: decision});
-  }, next);
-});
-
-app.get('/listTokens', function(req, res, next) {
-  // Returns all documents in db where user matches the
-  // x-remote-user header as an application/json array.
-  let user = req.headers[constants.USER_ID_HEADER];
-
-  model.listTokens(user).then(function(docs) {
-    res.status(200).json(docs);
-  }, next);
-});
+authorisation_controller.setup(app);
 
 app.use(express.static(path.join(__dirname, 'sentry/public')));
 
@@ -151,3 +92,7 @@ app.use(function(err, req, res, next) {
 
 serv.listen(port);
 logger.info(`npg_sentry started on port ${port}`);
+
+if ( module.parent ) {
+  module.exports = serv;
+}
