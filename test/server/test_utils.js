a5926eed156664068dd18e686c155b4c89c7e2b3
jmtcsngr <jmtc@sanger.ac.uk>
2017-03-14 11:46:32 +0000

    add util to generate self signed certs

--- test/server/test_utils.js
+++ test/server/test_utils.js
@@ -6,6 +6,27 @@ const pem = require('pem');
 const KEY_EXT = 'key';
 const CERT_EXT = 'cert';
 
+let create_self_signed_cert = ( path, cert_prefix, callback) => {
+  fse.ensureDirSync(path);
+
+  pem.createCertificate({
+    days:       2,
+    selfSigned: true
+  }, ( err, cert ) => {
+    if ( err ) {
+      callback( err );
+    }
+    try {
+      fse.writeFileSync(`${path}/${cert_prefix}.${CERT_EXT}`, cert.certificate);
+      fse.writeFileSync(`${path}/${cert_prefix}.${KEY_EXT}`, cert.serviceKey);
+    } catch (e) {
+      callback( e );
+    }
+
+    callback( null, cert );
+  });
+};
+
 let create_certificates = (path, ca_prefix, cert1_prefix, cert2_prefix, callback) => {
   fse.ensureDirSync(path);
 
@@ -20,7 +41,11 @@ let create_certificates = (path, ca_prefix, cert1_prefix, cert2_prefix, callback
     }
     let ca_cert = cert0;
 
-    fse.writeFileSync( `${path}/${ca_file_name}`, ca_cert.certificate );
+    try {
+      fse.writeFileSync( `${path}/${ca_file_name}`, ca_cert.certificate );
+    } catch ( e ) {
+      callback( e );
+    }
 
     let certInfo = {
       commonName: 'localhost',
@@ -35,8 +60,12 @@ let create_certificates = (path, ca_prefix, cert1_prefix, cert2_prefix, callback
         callback(error);
       }
 
-      fse.writeFileSync(`${path}/${cert1_prefix}.${CERT_EXT}`, cert1.certificate);
-      fse.writeFileSync(`${path}/${cert1_prefix}.${KEY_EXT}`, cert1.clientKey);
+      try {
+        fse.writeFileSync(`${path}/${cert1_prefix}.${CERT_EXT}`, cert1.certificate);
+        fse.writeFileSync(`${path}/${cert1_prefix}.${KEY_EXT}`, cert1.clientKey);
+      } catch ( e ) {
+        callback( e );
+      }
 
       let certInfo2 = {
         commonName: 'localhost',
@@ -51,8 +80,12 @@ let create_certificates = (path, ca_prefix, cert1_prefix, cert2_prefix, callback
           callback(error);
         }
 
-        fse.writeFileSync(`${path}/${cert2_prefix}.${CERT_EXT}`, cert2.certificate);
-        fse.writeFileSync(`${path}/${cert2_prefix}.${KEY_EXT}`, cert2.clientKey);
+        try {
+          fse.writeFileSync(`${path}/${cert2_prefix}.${CERT_EXT}`, cert2.certificate);
+          fse.writeFileSync(`${path}/${cert2_prefix}.${KEY_EXT}`, cert2.clientKey);
+        } catch ( e ) {
+          callback( e );
+        }
 
         callback(null, cert0, cert1, cert2);
       });
@@ -61,16 +94,23 @@ let create_certificates = (path, ca_prefix, cert1_prefix, cert2_prefix, callback
 };
 
 module.exports = {
-  create_certificates: create_certificates
+  create_certificates,
+  create_self_signed_cert
 };
-
 
 if ( !module.parent ) {
   create_certificates('./', 'CA', 'cert1', 'cert2', (error) => {
     if ( error ) {
       console.err(`Failed: ${error}`);
     } else {
-      console.log('finished');
+      console.log('finished certificate chain');
+    }
+  });
+  create_self_signed_cert('./', 'self_signed', (error) => {
+    if ( error ) {
+      console.err(`Failed: ${error}`);
+    } else {
+      console.log('finished self signed');
     }
   });
 }
