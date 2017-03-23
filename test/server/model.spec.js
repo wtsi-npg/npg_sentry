a681ea50fca212357bd15517945b7047b0da0ad8
jmtcsngr <jmtc@sanger.ac.uk>
2017-03-23 12:27:10 +0000

    introduce constants for operations

--- test/server/model.spec.js
+++ test/server/model.spec.js
@@ -141,6 +141,7 @@ describe('model', function() {
           expect(moment(hist.time)
             .isBetween(moment().subtract(5, 'seconds'), moment())).toBe(true);
           expect(hist.operating_user).toBe(user);
+          expect(hist.operation).toBe(constants.TOKEN_OPERATION_CREATE);
           expect(hist.reason).toBe('test creation');
         };
 
@@ -227,6 +228,7 @@ describe('model', function() {
           expect(moment(hist.time)
             .isBetween(moment().subtract(5, 'seconds'), moment())).toBe(true);
           expect(hist.operating_user).toBe(user);
+          expect(hist.operation).toBe(constants.TOKEN_OPERATION_REVOKE);
           expect(hist.reason).toBe('Test revocation');
         }).then(done, done.fail);
       });
